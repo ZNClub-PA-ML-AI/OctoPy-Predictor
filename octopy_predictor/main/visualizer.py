753e9548677aadcd761feec0ae4c5cc48cd7ba5a
ZNevzz <dsouza.nevil45@gmail.com>
2018-04-13 12:05:17 +0530

    TypeError: 'int' object is not iterable

--- octopy_predictor/main/visualizer.py
+++ octopy_predictor/main/visualizer.py
@@ -10,22 +10,28 @@ class Visualizer(object):
 	def __init__(self, arg):
 		super(Visualizer, self).__init__()
 		self.arg = arg
+		self.colors = ['#D2B4DE','#D68910','#58D68D','#633974','#34495E']
 
-	def time_series(self, df):
+	def time_series(self, df, columnLines):
 
 		df = df.fillna(0)
-		df.set_index('date')
+		df.set_index('date', inplace= True)
 		df.index = pd.to_datetime(df.index)
-		df.index.name = 'Date'
 		df.sort_index(inplace=True)
-
-		#df['Total'] = df.Yes + df['Not sure'] + df.No + df.Others
-		#df['Precision'] = round(df.Yes/df.Total, 2)
-
 		source = ColumnDataSource(df)
-
 		p = figure(x_axis_type="datetime", plot_width=800, plot_height=350)
-		p.line('Date', 'open', source=source)
+		for index in len(columnLines):
+			p.line('date', columnLines[index], source=source, color= self.colors[index])
 
 		output_file("ts.html")
 		show(p)
+
+'''
+from visualizer import Visualizer as viz
+from datagatherer import DataGatherer as dg
+PATH = r'..\data\nifty50.csv'
+df = dg.read(dg, PATH)
+columnDtypeGroups =  df.columns.to_series().groupby(df.dtypes).groups
+dtypeColumnPairs = {k.name,v for k,v in columnDtypeGroups.items()}
+viz.time_series(viz, df, dtypeColumnPairs['float64'])
+'''
