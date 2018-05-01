2b710efe2b842ada920ac6694aa76ff9664e564e
ZNevzz <dsouza.nevil45@gmail.com>
2018-05-01 19:47:01 +0530

    shutdown via endpoint implemented

--- octopy_predictor/main/web_runner.py
+++ octopy_predictor/main/web_runner.py
@@ -10,6 +10,18 @@ def hello_world():
    return 'Hello, Welcome to Flask demo!'
 
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+
 if __name__ == '__main__':
 
     context = init_dependencies()
