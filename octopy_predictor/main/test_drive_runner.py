bff845200fc5e5e4bca5837177973c695cbfa6b3
ZNevzz <dsouza.nevil45@gmail.com>
2018-09-06 10:53:12 +0530

    broken visualizer

--- octopy_predictor/main/test_drive_runner.py
+++ octopy_predictor/main/test_drive_runner.py
@@ -2,5 +2,21 @@
 
 import context
 
-viz = context['visualizer']
-gat = context['datagatherer']
+context.init_dependencies()
+
+viz = context.app_context['visualizer']
+gat = context.app_context['datagatherer']
+
+PATH = r'..\data\titanic-test.csv'
+PATH = r'..\data\nifty50.csv'
+
+df = gat.read(PATH)
+
+column_dtype_groups = df.columns.to_series().groupby(df.dtypes).groups
+for k,v in column_dtype_groups.items():
+	print(k,v)
+column_dtype_pairs = {k.name:v for k,v in column_dtype_groups.items()}
+viz.time_series(df, column_dtype_pairs['float64'])
+
+
+print('---->End of Test Drive<----')
