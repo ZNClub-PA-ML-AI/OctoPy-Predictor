894601da7b359c1f86fb3a7ca0b1d53e4d1627b9
ZNevzz <dsouza.nevil45@gmail.com>
2018-09-07 09:43:57 +0530

    time series output present

--- octopy_predictor/main/test_drive_runner.py
+++ octopy_predictor/main/test_drive_runner.py
@@ -12,11 +12,10 @@ PATH = r'..\data\nifty50.csv'
 
 df = gat.read(PATH)
 
+'''time_series'''
 column_dtype_groups = df.columns.to_series().groupby(df.dtypes).groups
-for k,v in column_dtype_groups.items():
-	print(k,v)
-column_dtype_pairs = {k.name:v for k,v in column_dtype_groups.items()}
-viz.time_series(df, column_dtype_pairs['float64'])
+column_dtype_dict = {k.name:v for k,v in column_dtype_groups.items()}
+viz.time_series(df, column_dtype_dict['float64'])
 
 
 print('---->End of Test Drive<----')
--- octopy_predictor/main/visualizer.py
+++ octopy_predictor/main/visualizer.py
@@ -1,56 +1,50 @@
 import pandas as pd
 
+
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show, output_file
 
 from matplotlib import pyplot as plt
 
 class Visualizer(object):
-	"""docstring for Visualizer"""
-	def __init__(self, arg = None):
-		super(Visualizer, self).__init__()
-		self.arg = arg
-		self.colors = ['#D2B4DE','#D68910','#58D68D','#633974','#34495E']
+    """docstring for Visualizer"""
+    def __init__(self, arg = None):
+        super(Visualizer, self).__init__()
+        self.arg = arg
+        self.colors = ['#D2B4DE','#D68910','#58D68D','#633974','#34495E']
 
-	def time_series(self, df, columnLines):
+    def time_series(self, df, columns):
 
-		df = df.fillna(0)
-		df.set_index('date', inplace= True)
-		df.index = pd.to_datetime(df.index)
-		df.sort_index(inplace=True)
-		source = ColumnDataSource(df)
-		p = figure(x_axis_type="datetime", plot_width=800, plot_height=350)
-		for index in len(columnLines):
-			p.line('date', columnLines[index], source=source, color= self.colors[index])
+        df = df.fillna(0)
+        df.set_index('date', inplace= True)
+        df.index = pd.to_datetime(df.index)
+        df.sort_index(inplace=True)
+        source = ColumnDataSource(df)
+        df['date'] = df.index
+        p = figure(x_axis_type="datetime", plot_width=1200, plot_height=350)
+        for index, column in enumerate(columns):
+            p.line('date', column, source=source, color= self.colors[index])
 
-		output_file("ts.html")
-		show(p)
-	'''
-	from visualizer import Visualizer as viz
-	from datagatherer import DataGatherer as dg
-	PATH = r'..\data\nifty50.csv'
-	df = dg.read(dg, PATH)
-	columnDtypeGroups =  df.columns.to_series().groupby(df.dtypes).groups
-	dtypeColumnPairs = {k.name,v for k,v in columnDtypeGroups.items()}
-	viz.time_series(viz, df, dtypeColumnPairs['float64'])
-	'''
-	def histogram(self, df, columns = ['Age']):
-		fig = plt.figure()
-		ax = fig.add_subplot(1, 1, 1)
+        output_file("time_series.html")
+        show(p)
+    
+    def histogram(self, df, columns = ['Age']):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
 
-		print(columns)
-		ax.hist(df[columns],bins = 10)
-		plt.title('Age distribution')
-		plt.xlabel('Age')
-		plt.ylabel('#Employee')
-		plt.show()
-	'''
-	from visualizer import Visualizer
+        print(columns)
+        ax.hist(df[columns],bins = 10)
+        plt.title('Age distribution')
+        plt.xlabel('Age')
+        plt.ylabel('#Employee')
+        plt.show()
+    '''
+    from visualizer import Visualizer
 
-	from datagatherer import DataGatherer
-	PATH = r'..\data\titanic-test.csv'
-	dg = DataGatherer()
-	df = dg.read(dg, PATH)
-	viz = Visualizer()
-	viz.histogram(df, ['Fare'])
-	'''
+    from datagatherer import DataGatherer
+    PATH = r'..\data\titanic-test.csv'
+    dg = DataGatherer()
+    df = dg.read(dg, PATH)
+    viz = Visualizer()
+    viz.histogram(df, ['Fare'])
+    '''
