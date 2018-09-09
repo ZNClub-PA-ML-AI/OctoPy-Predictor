118e386e99cfc900c23be08bf89da96c701b7743
ZNevzz <dsouza.nevil45@gmail.com>
2018-09-09 10:50:15 +0530

    Histogram resolved

--- octopy_predictor/main/visualizer.py
+++ octopy_predictor/main/visualizer.py
@@ -16,35 +16,37 @@ class Visualizer(object):
     def time_series(self, df, columns):
 
         df = df.fillna(0)
+        
         df.set_index('date', inplace= True)
         df.index = pd.to_datetime(df.index)
         df.sort_index(inplace=True)
-        source = ColumnDataSource(df)
         df['date'] = df.index
+       
         p = figure(x_axis_type="datetime", plot_width=1200, plot_height=350)
+        source = ColumnDataSource(df)
         for index, column in enumerate(columns):
             p.line('date', column, source=source, color= self.colors[index])
 
         output_file("time_series.html")
         show(p)
     
-    def histogram(self, df, columns = ['Age']):
-        fig = plt.figure()
-        ax = fig.add_subplot(1, 1, 1)
+    def _is_valid_histogram_input(self, df, column):
+        return column is not None\
+        and column in set(df.columns.values.tolist())
+    
+    def histogram(self, df, column = None, bins = 5):
+        if self._is_valid_histogram_input(df, column):
+            
+            df = df.fillna(0)
+            
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1)
+            ax.hist(df[column].tolist(), bins)
+            
+            plt.title(column + ' Distribution')
+            plt.xlabel(column)
+            plt.ylabel('Count')
+            plt.show()
 
-        print(columns)
-        ax.hist(df[columns],bins = 10)
-        plt.title('Age distribution')
-        plt.xlabel('Age')
-        plt.ylabel('#Employee')
-        plt.show()
-    '''
-    from visualizer import Visualizer
-
-    from datagatherer import DataGatherer
-    PATH = r'..\data\titanic-test.csv'
-    dg = DataGatherer()
-    df = dg.read(dg, PATH)
-    viz = Visualizer()
-    viz.histogram(df, ['Fare'])
-    '''
+    
+    
