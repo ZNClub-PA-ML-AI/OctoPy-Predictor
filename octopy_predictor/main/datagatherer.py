153397ed9c88257f8195847587398119e85e198a
ZNevzz <dsouza.nevil45@gmail.com>
2019-05-04 23:17:20 +0530

    updated gatherer

--- octopy_predictor/main/datagatherer.py
+++ octopy_predictor/main/datagatherer.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 from io import StringIO
+from collections import namedtuple
+
 from util import logit
 import util
 
@@ -11,6 +13,7 @@ class DataGatherer(object):
         self.arg = arg
 
     @logit
+    @staticmethod
     def _read_from_file(file):
         _file_content = None
         try:
@@ -23,15 +26,61 @@ class DataGatherer(object):
             return _file_content
     
     @logit
+    @staticmethod
+    def _determine_resource(path):
+        resource_type, file_type = None, None
+        
+        # resource type
+        resource_type = 'web' if path.startswith('http') else 'local'
+        
+        # file type
+        try:
+            file_extension_index = path.rindex('.')
+        except ValueError as val_error:
+            # TODO: message = invalid path
+            raise val_error
+        else:
+            file_type = path[file_extension_index + 1 :]
+        finally:
+            FileResource = namedtuple('FileResource', 'resource_type file_type')
+            return FileResource(resource_type = resource_type, file_type = file_type)
+    
+    
+    @logit
+    @staticmethod
+    def _read_from_path(path):
+        '''
+        read data from a file available at given path
+        '''
+        df = pd.DataFrame()
+        metadata = _determine_resource(path)
+        
+        if metadata.resource_type == 'local':
+            
+            if metadata.file_type == 'csv':
+                df = pd.read_csv(path)
+            
+        elif metadata.resource_type == 'web':
+            
+            if metadata.file_type == 'csv':
+                df = pd.read_csv(path)
+        
+        return df
+                
+    @logit
     def read(self, path = None, file = None):
+        '''
+        read receives either path or file. If received both, file is given priority
+        '''
         try:            
             df = None
-            if file is None:
-                df = pd.read_csv(path, nrows=10000)
-            elif path is None:
+            if path is None:
                 file_content = self._read_from_file(file)
                 util.debug_store['StringIO(file_content) at datagatherer'] = StringIO(file_content)                
                 df = pd.read_csv(StringIO(file_content))
+                
+            elif file is None:
+                df = pd.read_csv(path)
             else:
                 raise ValueError
         except ValueError as value_error:
